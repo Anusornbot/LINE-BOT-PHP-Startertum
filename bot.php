6402be0ece8e4951bbda01e4ecdc88f68eb35a2b
Anusornbot <tomyamtam.tum@gmail.com>
2016-12-27 10:03:29 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,4 +1,6 @@
 <?php
+$proxy = 'http://fixie:1FtpXYX8q4TvvYD@velodrome.usefixie.com:80';
+$proxyauth = 'username:password';
 $access_token = 'Bl/DCBKhbMBiStGbVgEJA6TNE47XTHavgS2e+VMsTg/etHe6Kjo2M1aUNS9Pd5ao491su45EnF2HsimGDS0arCLu/pl4dwplYAWANkxc8559dhbyOVdoU7qEj+niND7qkuF8r/OmuopxvCNvYhimYQdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
@@ -37,6 +39,8 @@ if (!is_null($events['events'])) {
 			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
 			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			curl_setopt($ch, CURLOPT_PROXY, $proxy);
+			curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
 			$result = curl_exec($ch);
 			curl_close($ch);
 
